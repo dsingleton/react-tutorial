fe8fe36a823e5b1e7eea2e0ad4a64b8ed3a0092a
David Singleton <david@dsingleton.co.uk>
2016-06-03 19:58:26 +0100

    Static components

---
+++ public/scripts/tutorial.js
@@ -0,0 +1,36 @@
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Comment List
+      </div>
+    )
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Comment Box
+      </div>
+    )
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById("content")
+);
